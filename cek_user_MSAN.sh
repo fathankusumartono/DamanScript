b1305e0ae1bab3a16be7257087438bdad9708457
Fathan Kusumartono <fathankusumartono@users.noreply.github.com>
2017-06-15 11:18:14 +0700

    Create cek_user_MSAN.sh

---
+++ cek_user_MSAN.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/expect
+set timeout 3
+set hostName [lindex $argv 0]
+set logDefault "zte"
+set i 1
+set x 1
+
+proc showShelf {shelf slot} {
+	expect "#"
+	send "show msag sip user shelf $shelf $slot\r"
+
+	expect {
+	  " --More--" {
+	  send " "
+	  exp_continue
+	 }
+	 "#" {
+	  send_user "Ganti shelf!\n"
+	 }
+	}
+}
+
+spawn telnet $hostName
+expect "Username:"
+send "$logDefault\r"
+expect "Password:"
+send "$logDefault\r"
+
+while {$x < 3} {
+  while {$i < 21} {
+	showShelf $x $i
+	set i [expr $i+1]
+  }
+  set x [expr $x+1]
+  set i 1
+  #send_user "x=$x; i=$i"
+}
+
+expect "#"
+send "exit\r"
+interact
